5077fd7b6ce6523c0001547230772bb8b35a8ddb
Zimbelstern <post@zimbelstern.eu>
2025-07-05 15:36:51 +0200

    Upgraded Gradle, AGP, Kotlin, JVM target, compileSdk, targetSdk, dependencies & tweaked build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,24 @@
-import kotlin.sequences.sorted
-
 plugins {
 	id("com.android.application")
-	id("com.github.triplet.play") version "3.10.1"
-	id("com.google.devtools.ksp") version "2.1.20-2.0.0"
+	id("com.github.triplet.play") version "3.12.1"
+	id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 	id("kotlin-android")
 	id("kotlin-kapt")
 	id("kotlin-parcelize")
 	id("org.jetbrains.kotlin.android")
-	id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
+	id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
 }
 
 android {
-	compileSdk = 35
+	compileSdk = 36
+	namespace = "eu.zimbelstern.tournant"
 
 	defaultConfig {
 		applicationId = "eu.zimbelstern.tournant"
 		minSdk = 21
-		targetSdk = 35
+		targetSdk = 36
 		versionCode = 36
 		versionName = "2.10.1"
-
-		ksp {
-			arg("room.schemaLocation", "$projectDir/schemas")
-		}
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables.generatedDensities?.clear()
@@ -37,10 +32,10 @@ android {
 		}
 		.plus(File("$projectDir/src/main/res/values"))
 		.sorted()
-		.map {
-			it.walk().find { it.name == "strings.xml" }!!.readText().run {
+		.map { localeDir ->
+			localeDir.walk().find { it.name == "strings.xml" }!!.readText().run {
 				listOf(
-					it.name.drop(7).replace("-r", "-").ifEmpty { "en" },
+					localeDir.name.drop(7).replace("-r", "-").ifEmpty { "en" },
 					substringAfter("<string name=\"to\">", "").substringBefore("</string>"),
 					substringAfter("<string name=\"hours_for_regex\">", "").substringBefore("</string>"),
 					substringAfter("<string name=\"minutes_for_regex\">", "").substringBefore("</string>"),
@@ -49,16 +44,11 @@ android {
 			}
 		}
 
-
 	applicationVariants.configureEach {
 		resValue("string", "versionName", versionName)
 		resValue("string", "availableLanguages", availableLanguages.map { it.first() }.joinToString(","))
 		resValue("string", "localisedTimeStrings", availableLanguages.map { it.joinToString(":") }.joinToString(";"))
 		mergedFlavor.manifestPlaceholders["fileprovider_authority"] = "$applicationId.fileprovider"
-	}
-
-	androidResources {
-		generateLocaleConfig = true
 	}
 
 	buildFeatures {
@@ -97,41 +87,38 @@ android {
 		}
 	}
 
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_17
-		targetCompatibility = JavaVersion.VERSION_17
+	kotlin {
+		jvmToolchain(21)
 	}
 
-	kotlinOptions {
-		jvmTarget = "17"
+	ksp {
+		arg("room.schemaLocation", "$projectDir/schemas")
 	}
-
-	namespace = "eu.zimbelstern.tournant"
 }
 
 dependencies {
 	implementation("androidx.activity:activity-ktx:1.10.1")
-	implementation("androidx.appcompat:appcompat:1.7.0")
+	implementation("androidx.appcompat:appcompat:1.7.1")
 	implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 	implementation("androidx.core:core-ktx:1.16.0")
 	implementation("androidx.core:core-splashscreen:1.0.1")
 	implementation("androidx.exifinterface:exifinterface:1.4.1")
-	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
 	implementation("androidx.paging:paging-runtime-ktx:3.3.6")
 	implementation("androidx.preference:preference-ktx:1.2.1")
 	implementation("com.google.android.material:material:1.12.0")
 	implementation("com.google.android.flexbox:flexbox:3.0.0")
-	implementation("io.coil-kt.coil3:coil:3.1.0")
+	implementation("io.coil-kt.coil3:coil:3.2.0")
 
-	val composeVersion = "1.8.1"
-	implementation("androidx.compose.material:material-icons-extended-android:1.7.8")
+	val composeVersion = "1.8.3"
 	implementation("androidx.compose.material:material:$composeVersion")
+	implementation("androidx.compose.material:material-icons-extended-android:1.7.8")
 	implementation("androidx.compose.ui:ui:$composeVersion")
 
-	val roomVersion = "2.7.1"
+	val roomVersion = "2.7.2"
 	implementation("androidx.room:room-ktx:$roomVersion")
-	implementation("androidx.room:room-runtime:$roomVersion")
 	implementation("androidx.room:room-paging:$roomVersion")
+	implementation("androidx.room:room-runtime:$roomVersion")
 	ksp("androidx.room:room-compiler:$roomVersion")
 
 	val moshiVersion = "1.15.2"
@@ -143,10 +130,10 @@ dependencies {
 	implementation("io.noties.markwon:core:$markwonVersion")
 	implementation("io.noties.markwon:html:$markwonVersion")
 
-	implementation(kotlin("reflect"))
+	implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.0")
 
 	testImplementation("androidx.test.ext:junit-ktx:1.2.1")
-	testImplementation("org.robolectric:robolectric:4.14.1")
+	testImplementation("org.robolectric:robolectric:4.15.1")
 
 	androidTestImplementation("androidx.test:runner:1.6.2")
 	androidTestImplementation("androidx.test:rules:1.6.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("com.android.tools.build:gradle:8.9.2")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+		classpath("com.android.tools.build:gradle:8.11.0")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
 	}
 }
 
