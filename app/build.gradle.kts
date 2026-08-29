5f9730e1fa1875eab3f6dbce211c0a5d9fbf3ada
Zimbelstern <post@zimbelstern.eu>
2026-08-29 18:18:59 +0200

    Version 2.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
 		applicationId = "eu.zimbelstern.tournant"
 		minSdk = 21
 		targetSdk = 36
-		versionCode = 41
-		versionName = "2.11.1"
+		versionCode = 42
+		versionName = "2.12.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables.generatedDensities?.clear()
